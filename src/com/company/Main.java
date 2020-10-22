da02caf555c3f2921dc05773e241ac302d6d1dfc
Mykhailo Popovych <emubis2001@gmail.com>
2020-10-22 19:29:18 +0300

    Commit TaskAdressName

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-
-    private static final double Pi =3.14;
-
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Input radius:");
-        double radius = Double.parseDouble(br.readLine());
-        System.out.println("radius is" + radius);
-
-        double area = Pi * Math.pow(radius,2);
-        double perimeter = 2 * Pi * radius;
-
-        System.out.println("Area of flower bed: " + area);
-        System.out.println("Perimeter of flower bed: " + perimeter);
+        System.out.println("What's your name ?");
+        System.out.print("Input your name: ");
+        String name = br.readLine();
+        System.out.println("Where do you  live? ");
+        System.out.print("Input your address: ");
+        String address = br.readLine();
+        System.out.print("Name is: " + name + " Address is: " + address);
     }
 }
 
