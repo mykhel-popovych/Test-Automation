5c286a98018437d541514d878ca7289479ca5865
Mykhailo Popovych <emubis2001@gmail.com>
2020-10-22 18:47:32 +0300

    Commit Number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,29 +8,46 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("a:");
-        double a = Double.parseDouble(br.readLine());
-        System.out.println("b:");
-        double b = Double.parseDouble(br.readLine());
-        System.out.println("c:");
-        double c = Double.parseDouble(br.readLine());
-        System.out.println("x:");
-        double x = Double.parseDouble(br.readLine());
-        System.out.println("y:");
-        double y = Double.parseDouble(br.readLine());
+        System.out.print("Enter n:");
+        int n = Integer.parseInt(br.readLine());
+        int squareN = (int)(Math.pow(n,2));
+        System.out.println("Square of n:" + squareN);
+        String strSquareN = Integer.toString(squareN);
 
-        if((a <= x && b <= y) || (a <= y && b <= x)){
-            System.out.println("Можна просунути");
+        char[] result = strSquareN.toCharArray();
+        char numberThree = '3';
+        String message = null;
+        for(int i = 0; i < result.length; i++){
+            if(numberThree == result[i]){
+                message = "Number is in this string!";
+                break;
+            }
+            else{
+                message = "Number isn't in this string!";
+            }
         }
-        else if((a <= x && c <= y) || (a <= y && c <= x)){
-            System.out.println("Можна просунути");
-        }
-        else if((c <= x && b <= y) || (c <= y && b <= x)){
-            System.out.println("Можна просунути");
+        System.out.println(message);
+
+        System.out.print("Reversed number: ");
+        StringBuilder reversedNumber = new StringBuilder(Integer.toString(n));
+        System.out.println(reversedNumber.reverse().toString());
+
+        char [] result2 = Integer.toString(n).toCharArray();
+
+        if(result2.length <= 1){
+            System.out.print("Number after permutation: " + n);
         }
         else{
-            System.out.println("Неможна просунути");
+            char temp = result2[0];
+            result2[0] = result2[result2.length - 1];
+            result2[result2.length - 1] = temp;
+            System.out.print("Number after permutation: ");
+            for(int i = 0; i < result2.length; i++){
+                System.out.print(result2[i]);
+            }
+            System.out.println("\n");
         }
+        System.out.print("Number with one number on the start and end:" + "1" + n + "1");
     }
 }
 
