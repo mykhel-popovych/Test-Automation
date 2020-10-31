02b2116813508d83f3ae534d7d161edc9c20da4b
Mykhailo_Popovych <55396640+mykhel-popovych@users.noreply.github.com>
2020-10-31 23:30:01 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+class BrickSize{
+    private double a;
+    private double b;
+    private double c;
 
+    public BrickSize(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public double getA(){
+        return a;
+    }
+    public double getB(){
+        return b;
+    }
+    public double getC(){
+        return c;
+    }
+}
+class HoleSize{
+    private double x;
+    private double y;
+
+    public HoleSize(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public double getX(){
+        return x;
+    }
+    public double getB(){
+        return y;
+    }
+}
+
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("a:");
@@ -18,19 +53,25 @@ public class Main {
         double x = Double.parseDouble(br.readLine());
         System.out.println("y:");
         double y = Double.parseDouble(br.readLine());
+        BrickSize brickSize = new BrickSize(a,b,c);
+        HoleSize holeSize = new HoleSize(x,y);
 
-        if((a <= x && b <= y) || (a <= y && b <= x)){
+        if((brickSize.getA() <= holeSize.getX() && brickSize.getB() <= holeSize.getB()) ||
+                (brickSize.getA() <= holeSize.getB() && brickSize.getB() <= holeSize.getX())){
             System.out.println("Можна просунути");
         }
-        else if((a <= x && c <= y) || (a <= y && c <= x)){
+        else if((brickSize.getA() <= holeSize.getX() && brickSize.getC() <= holeSize.getB()) ||
+                (brickSize.getA() <= holeSize.getB() && brickSize.getC() <= holeSize.getX())){
             System.out.println("Можна просунути");
         }
-        else if((c <= x && b <= y) || (c <= y && b <= x)){
+        else if((brickSize.getC() <= holeSize.getX() && brickSize.getB() <= holeSize.getB()) ||
+                (brickSize.getC() <= holeSize.getB() && brickSize.getB() <= holeSize.getX())){
             System.out.println("Можна просунути");
         }
         else{
             System.out.println("Неможна просунути");
         }
     }
+
 }
 
