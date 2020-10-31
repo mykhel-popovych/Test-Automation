5cdb0c757c560c4c98172f5e43df2c1dfb389743
Mykhailo_Popovych <55396640+mykhel-popovych@users.noreply.github.com>
2020-11-01 00:23:32 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,25 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+class Solve_Tasks{
+    private int n;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter n:");
-        int n = Integer.parseInt(br.readLine());
-        int squareN = (int)(Math.pow(n,2));
+    public Solve_Tasks(int n){
+        this.n = n;
+    }
+
+    public void Solution(int n) {
+        int squareN = (int) (Math.pow(n, 2));
         System.out.println("Square of n:" + squareN);
         String strSquareN = Integer.toString(squareN);
 
         char[] result = strSquareN.toCharArray();
         char numberThree = '3';
         String message = null;
-        for(int i = 0; i < result.length; i++){
-            if(numberThree == result[i]){
+        for (int i = 0; i < result.length; i++) {
+            if (numberThree == result[i]) {
                 message = "Number is in this string!";
                 break;
-            }
-            else{
+            } else {
                 message = "Number isn't in this string!";
             }
         }
@@ -48,6 +49,16 @@ public class Main {
             System.out.println("\n");
         }
         System.out.print("Number with one number on the start and end:" + "1" + n + "1");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter n: ");
+        int n = Integer.parseInt(br.readLine());
+        Solve_Tasks solve = new Solve_Tasks(n);
+        solve.Solution(n);
     }
 }
 
