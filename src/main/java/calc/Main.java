2847337e09316204f4e4ba8c8eb8c9397911563a
Mykhailo_Popovych <55396640+mykhel-popovych@users.noreply.github.com>
2020-11-14 16:44:35 +0200

    Update Main.java

--- src/main/java/calc/Main.java
+++ src/main/java/calc/Main.java
@@ -1,44 +1,28 @@
 package calc;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 class NotInRangeException extends Exception{
+
     private int number;
 
     public NotInRangeException(String s) {
-        super(s);
     }
 
     public int getNumber(){return number;}
-    public NotInRangeException(String message, int num){
-
+    public NotInRangeException(String message, int number){
         super(message);
-        number = num;
+        this.number = number;
     }
 }
 
 public class Main {
-    public static void arrayBuild() throws NotInRangeException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int array[] = new int[10];
-        for(var i = 0; i < array.length; i++){
-            System.out.print("Write " + (i + 1) + " element of array: ");
-            array[i] += Integer.parseInt(br.readLine());
-        }
-        for(int c: array){
-            if(c <= 1 || c >= 100){
-                throw new NotInRangeException("Write numbers between 1 and 100");
-            }
-        }
-        System.out.print("Sorted array is: ");
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
-    }
-
     public static void main(String[] args) throws Exception {
         double num1 = 0, num2 = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -58,9 +42,37 @@ public class Main {
         try{
             System.out.println("Dividing: " + obj.div(num1,num2));
         }catch(Exception ex){
-            ex.getMessage();
+            System.out.println(ex.getMessage());
         }
-        System.out.println("Number is : " + obj.readNumber(1,10));
-        arrayBuild();
+        int resultReadNumber = 0;
+        try{
+            resultReadNumber = obj.readNumber(1,10);
+        }catch(NumberFormatException | NotInRangeException ex){
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("Number is : " + resultReadNumber);
+        try{
+            arrayBuild();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+    static void arrayBuild() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> people = new ArrayList<Integer>();
+        for(int i = 0; i < 10; i++){
+            System.out.print("Write " + (i + 1) + " element of array: ");
+            int variable = Integer.parseInt(br.readLine());
+            if(people.contains(variable)){
+                throw new Exception("All numbers must be different!!");
+            }else if(variable <= 1 || variable >= 100){
+                throw new Exception("Write numbers between 1 and 100");
+            }else{
+                people.add(variable);
+            }
+        }
+        Collections.sort(people);
+        System.out.print(people);
     }
 }
