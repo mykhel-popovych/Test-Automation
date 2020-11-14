9c9e5e585858330fa530d10911e46a066c4991d5
Mykhailo_Popovych <55396640+mykhel-popovych@users.noreply.github.com>
2020-11-14 16:43:02 +0200

    Update DivReadNumber.java

--- src/main/java/calc/DivReadNumber.java
+++ src/main/java/calc/DivReadNumber.java
@@ -1,5 +1,4 @@
 package calc;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,20 +11,31 @@ public class DivReadNumber {
         }
         return divideTwoNumbers;
     }
-    public static int readNumber(int start, int end) throws NotInRangeException, IOException {
+    public static int readNumber(int start, int end) throws NotInRangeException, Exception {
+        int countIfNumberTest = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int number = 0;
         System.out.print("Write Number: ");
-        try{
-            number = Integer.parseInt(br.readLine());
-        }catch(NumberFormatException ex){
-            System.out.println(ex.getMessage());
+        String number = br.readLine();
+        if(number.isEmpty()){
+            throw new NotInRangeException("Write some number!!!");
+        }else if(number.length() > 0){
+            char[] char1 = number.toCharArray();
+            for(char c : char1){
+                if(Character.isDigit(c)){
+                    countIfNumberTest++;
+                }
+            }
+            if(number.length() != countIfNumberTest){
+                throw new Exception("Write only Number!!!");
+            }else{
+                if(Integer.parseInt(number) < start || Integer.parseInt(number) > end){
+                    throw new NotInRangeException("Write in range between: " + start + " and " + end, Integer.parseInt(number));
+                }else{
+                    return Integer.parseInt(number);
+                }
+            }
         }
-        if(number < start || number > end){
-            throw new NotInRangeException("Write in range between: " + start + " and " + end);
-        }else{
-            return number;
-        }
+        return Integer.parseInt(number);
     }
     public static String readNumberForTest(int start, int end, int number){
         if(number < start || number > end){
